bb3f035d14414d7ebc190aef826c267e0e3fd9ce
Dale Tovar <daletovar@gmail.com>
2021-11-23 14:11:24 -0700

    add model to app

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
+import numpy as np
+import en_textcat_goemotions
 
-
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_model():
-    # loads the model
-    pass
+    return en_textcat_goemotions.load()
 
 def classify(text):
-    pass
+    out = nlp(text)
+    keys = list(out.cats.keys())
+    values = list(out.cats.values())
+    max_val = np.argmax(values)
+    return keys[max_val]
 
-model = load_model()
+nlp = load_model()
 
 title = st.title('Classify text')
 text = st.text_area('write a sentence to classify', key='text_content')
 
-out = classify(text)
+if text is not None:
+    category = classify(text)
+    st.markdown(f'This is {category} text')
 
